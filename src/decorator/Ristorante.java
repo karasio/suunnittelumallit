c9b9f8fe8e2b2b26bd24e4994ba4e9caa2270a2c
Katri Rasio <katri.rasio@metropolia.fi>
2020-03-31 17:11:27 +0300

    small refactoring

--- src/decorator/Ristorante.java
+++ src/decorator/Ristorante.java
@@ -5,15 +5,12 @@ import java.util.List;
 
 public class Ristorante {
     public static void main(String[] args) {
-        List<Pizza> menu = new ArrayList();
-        Pizza p1 = new Pineapple(new Chicken(new Feta(new Base())));
-        menu.add(p1);
-        Pizza p2 = new Tunafish(new Onion(new Mozzarella(new Base())));
-        menu.add(p2);
-        Pizza p3 = new Voner(new Olive(new Onion(new Base())));
-        menu.add(p3);
-        Pizza p4 = new Chicken(new Feta(new Mozzarella(new Base())));
-        menu.add(p4);
+        Pizza[] menu = {
+                new Pineapple(new Chicken(new Feta(new Base()))),
+                new Tunafish(new Onion(new Mozzarella(new Base()))),
+                new Voner(new Olive(new Onion(new Base()))),
+                new Chicken(new Feta(new Mozzarella(new Base())))
+        };
 
         System.out.println("Welcome to Marios! What may we get you?");
         System.out.println("-----------------------------------------------------------------------------------------------");
